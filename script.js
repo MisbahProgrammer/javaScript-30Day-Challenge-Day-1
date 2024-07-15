a895cbf6f57de5286617d8ed23d43abd19ef2e25
MisbahProgrammer <misbahrehman891@gmail.com>
2024-07-15 07:49:19 +0500

    JavaScript-30Days-Challenge-Day1

---
+++ script.js
@@ -0,0 +1,18 @@
+// DAY 1
+// Variables and Data Types
+var num = 3;
+console.log(num);
+let name = "misbah";
+console.log(name);
+const condition = true;
+console.log(condition);
+// different types of data
+var num = 3;
+console.log(typeof(num));
+let nickName = "misbah";
+console.log(typeof(name));
+const value = true;
+console.log(typeof(condition));
+let arr = ["Punjab","Sindh","Gilgit","Balochistan","Kpk"]
+console.log(typeof(arr))
+// in this challenge i learnt different data types and use of typeof() operator and use var,let and const.
